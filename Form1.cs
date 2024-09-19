ee3932c2ece574215e336951d59fd996977ec133
alosslessdev <ppaanncchhoo507@gmail.com>
2024-09-19 16:21:31 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1DS4
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        int aleatorio;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                aleatorio = random.Next(101);
+                label1.Text = aleatorio.ToString("D2");
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.MinDate = new DateTime(1900, 1, 01);
+            dateTimePicker1.MaxDate = DateTime.Today;
+        }
+
+
+        private void EliminarTexto(TextBox cajaTxt, String patron)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(cajaTxt.Text, patron))
+            {
+                cajaTxt.Text = cajaTxt.Text.Remove(cajaTxt.Text.Length - 1);
+                cajaTxt.SelectionStart = cajaTxt.Text.Length;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            EliminarTexto(textBox1, @"[^a-zA-Z]");
+
+
+        }
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1DS4
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string nombre, string edad, string sexo, string signo, string prediccion, string numeros)
+        {
+            InitializeComponent();
+            label1.Text = nombre;
+            label2.Text = edad;
+            label3.Text = sexo;
+            label4.Text = signo;
+            label5.Text = prediccion;
+            label6.Text = numeros;
+        }
+
+
+    }
+}
